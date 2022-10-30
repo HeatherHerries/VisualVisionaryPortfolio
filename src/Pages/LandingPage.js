1b5ed703a419af927c38144ccf4b70cf5d9dff1c
HeatherH <hherries@yahoo.com>
2022-10-30 13:16:05 -0600

    Added background color, image and social icons.

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LandingPage.css'
+import BgImage from '../Assets/Images/landing-bg-image.png'
 import FBIcon from '../Assets/Images/facebook-icon.svg'
 import LinkedInIcon from '../Assets/Images/linkedin-icon.svg'
 import TwitterIcon from '../Assets/Images/twitter-icon.svg'
@@ -7,12 +8,19 @@ import GitHubIcon from '../Assets/Images/github-icon.svg'
 
 function LandingPage() {
   return (
-    <div className='landing-page-container'>
-      <div className="social-icons-container">
-        <img src={FBIcon} alt="" className="si1" />
-        <img src={LinkedInIcon} alt="" className="si2" />
-        <img src={TwitterIcon} alt="" className="si3" />
-        <img src={GitHubIcon} alt="" className="si4" />
+    <div>
+      <div className='landing-page-container'>
+        <div className="bg-image-container">
+          <img className='bg-image'src={BgImage} alt="" />
+        </div>
+        <div className="social-container">
+          <img src={FBIcon} alt="" />
+          <img src={LinkedInIcon} alt="" />
+          <img src={TwitterIcon} alt="" />
+          <img src={GitHubIcon} alt="" />
+        </div>
+
+        
       </div>
     </div>
   )
