517b2189ee07d4cc99e3fc046991210d91e28813
HeatherH <hherries@yahoo.com>
2022-11-12 18:16:14 -0700

    Completed Vision Began section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -13,22 +13,26 @@ function AboutPage() {
 
       <div className="vision-began-container">
         <div className="vb-header">
-          <h2>How the Vision Began</h2>
+          <p>How the Vision Began</p>
         </div>
 
         <div className="vb-text-content">
           <p>Ever had one of those lightning-bolt-from-the-sky moments? I did. 
           It arrived unannounced, typical lightning bolt! 
           I was a hair stylist at the time with a solid clientele. I guess you could say I was on cruise control, stuck in limbo. Until I wasn’t...</p>
+          <br />
           <p>I have always craved a challenge and I needed a change. 
           So, I dove headfirst into an immersive full stack web development course. Adiós, dull routine, hello serious professional and creative freedom!
           </p>
+          <br />
           <p>
-          For the moment, hair styling still paid the bills, so I used that time hone into my logical side. and prepare myself for a new chapter. 
+          For the moment, hair styling still paid the bills, so I used that time  to hone into my logical side. and prepare myself for a new chapter. 
           I styled hair during the day and designed websites for friends and family after hours. The excitement and satisfaction were intoxicating. 
           </p>
-          <p>With all the logic under my belt now it was time for me to devour design. I learned everythig that I could get my little hands on about designing UI/UX to solve the worlds problems. Now, I am able  and craft digital masterpieces!. 
+          <br />
+          <p>With all the logic under my belt now, it was time for me to devour design. I learned everything that I could get my little hands on about designing UI/UX to solve the worlds problems. Now, I am able  and craft digital masterpieces!. 
           </p>
+          <br />
           <p>And since I’ve got your ear--
           Maybe we can change the world together.  Let’s collaborate. Or, check out my portfolio to see my work come to life. 
           </p>
@@ -36,12 +40,18 @@ function AboutPage() {
 
         <div className="vb-image">
           <img src={VBImage} alt="" />
-          <Link style={{textDecoration: 'none'}}to="#"><Button 
-        text="See My Resume"
-        /></Link>
         </div>
 
-        <SectionBreakLine class="vb-section-break-line"/>
+        <div className="vb-button">
+        <Link style={{textDecoration: 'none'}}to="#"><Button 
+        text="See My Resume"
+        /></Link> 
+        </div>
+
+        <div className="vb-section-break-line">
+        <SectionBreakLine />
+        </div>  
+        
       </div>
     </div>
   )
