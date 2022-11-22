fd459d69fa6ce819f380232b5405faed5e384612
HeatherH <hherries@yahoo.com>
2022-11-21 18:41:44 -0700

    Updated LandingPage.js.

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import '../Styles/LandingPage.css'
-import LpBackground from '../Components/LpBackground'
 import SocialLinks from '../Components/SocialLinks'
-import LpHeader from '../Components/LpHeader'
-import LpButton from '../Components/LpButtonSection'
+import LPImage from '../Assets/Images/lp-logo.svg'
+import Button from '../Components/Button'
+import { Link } from "react-router-dom"
 
 
 function LandingPage() {
   return (
-    <>
-      <LpBackground />
-      <SocialLinks />
-      <LpHeader />
-      <LpButton />
-    </>
+    <div className='landing-page-container'>
+      
+      <div className="lp-social-links">
+        <SocialLinks />
+      </div>
+
+      <div className="lp-content">
+        <img src={LPImage} alt="" />
+        <Link style={{textDecoration: 'none'}}to="/ContactPage"><Button 
+          text="Learn More"
+          /></Link> 
+      </div>
+    </div>
   )
 }
 
 export default LandingPage
+
