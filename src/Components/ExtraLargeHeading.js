a1a4333399db2f1780164a60257aef6126bf145d
Heather Herries <hherries@yahoo.com>
2022-11-22 12:53:17 -0700

    Update src/Components/ExtraLargeHeading.js
    Co-authored-by: Zach Herries <zach.herries@carta.com>

--- src/Components/ExtraLargeHeading.js
+++ src/Components/ExtraLargeHeading.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../Styles/ExtraLargeHeading.css'
 
 function ExtraLargeHeading({text}) {
-  return (<p className="ex-lrg-heading">{text}</p>)}
+  return <p className="ex-lrg-heading">{text}</p>;
+}
 
 export default ExtraLargeHeading
